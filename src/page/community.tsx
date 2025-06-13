843217208d834ef838def34dbd238b081a40fdf7
egileekada <egileoniso.ekada@gmail.com>
2025-06-14 00:06:57 +0100

    update on community

--- src/page/community.tsx
+++ src/page/community.tsx
@@ -14,42 +14,7 @@ export default function ChannelsPage() {
     const [show, setShow] = useState(false)
 
     return (
-        <LoadingAnimation loading={isLoading} >
-            {/* <div className=" w-full relative h-full bg-blue-400  " >
-                <div className=" w-full flex h-full inset-0 lg:flex-row flex-col gap-4 overflow-y-auto lg:overflow-y-hidden lg:gap-6 text-primary pb-6 lg:px-0 " >
-                    <div className=" w-full h-fit flex flex-col rounded-b-[44px] lg:rounded-[44px] lg:p-8 pb-2 " >
-                        <div className=" w-full h-[240px] bg-green-700 relative rounded-b-[44px] lg:rounded-[44px] " > 
-                            <img alt="image" src={item?.photo} className=" w-full h-full object-cover lg:rounded-[44px] " />
-                            <div className=" absolute inset-0 bg-black bg-opacity-40 z-[5] lg:rounded-[44px] " /> 
-                        </div>
-                        <div className=" w-full px-4 pb-6 lg:px-6 overflow-y-auto z-20 -mt-20 lg:-mt-[18%]  " >
-                            <div className=" p-5 text-primary w-full gap-2 bg-white flex flex-col rounded-[34px] items-center " style={{ boxShadow: "0px 3px 3px 0px #00000038" }} >
-                                <Text className=" font-black text-2xl " >{item?.name}</Text>
-                                <Text className=" text-xs text-primary text-opacity-75 font-semibold text-center " >{item?.description}</Text>
-
-                                {item?.members?.length > 0 && (
-                                    <div role="button" className='flex items-center mt-2 bg-[#37137F4D] px-3 rounded-full w-fit h-[40px] mx-auto text-black ' >
-                                        <div className=' w-7 h-7 rounded-full'>
-                                            <img src={item?.members[0]?.photo} alt={item?.members[0]?._id} className=" w-full h-full object-cover rounded-full " />
-                                        </div>
-                                        {item?.members?.length > 1 && (
-                                            <div className=' w-7 h-7 rounded-full -ml-2 ' >
-                                                <img src={item?.members[1]?.photo} alt={item?.members[1]?._id} className=" w-full h-full object-cover rounded-full " />
-                                            </div>
-                                        )}
-                                        {item?.members?.length > 2 && (
-                                            <div className=' w-7 h-7 rounded-full -ml-2 ' >
-                                                <img src={item?.members[2]?.photo} alt={item?.members[2]?._id} className=" w-full h-full object-cover rounded-full " />
-                                            </div>
-                                        )}
-                                        <Text className=' ml-2 font-semibold text-xs text-[#37137F] ' >{formatNumberWithK(item?.members?.length)} Members</Text>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+        <LoadingAnimation loading={isLoading} > 
             <div className=" w-full h-full p-4 flex flex-col gap-4 items-center " >
                 <div className=" w-full max-w-[500px] flex flex-col gap-4  " > 
                     <div className=" w-full h-[240px] relative rounded-b-[24px] rounded-[24px] "  >
