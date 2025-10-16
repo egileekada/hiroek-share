a63444b9f03e39c845397380aa16bac9be3e5e13
egileekada <egileoniso.ekada@gmail.com>
2025-10-16 10:37:13 -0300

    Refactor CountdownTimer component to remove commented-out code and improve readability.

--- src/components/countDownTimer.tsx
+++ src/components/countDownTimer.tsx
@@ -1,33 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 
-const CountdownTimer = ({ targetTime }: { targetTime: string }) => {
-    // const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-    // function calculateTimeLeft() {
-    //     // const difference = new Date(targetTime).getTime() - new Date().getTime();
-
-    //   const now = Date.now();
-    //   const diff = target - now;
-
-    //     if (difference > 0) {
-    //         return {
-    //             days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    //             hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-    //             minutes: Math.floor((difference / (1000 * 60)) % 60),
-    //             seconds: Math.floor((difference / 1000) % 60),
-    //         };
-    //     } else {
-    //         return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     const timer = setInterval(() => {
-    //         setTimeLeft(calculateTimeLeft());
-    //     }, 1000);
-
-    //     return () => clearInterval(timer); // Clean up on unmount
-    // }, [targetTime]);
+const CountdownTimer = ({ targetTime }: { targetTime: string }) => { 
 
     const [timeLeft, setTime] = useState({} as any);
     const intervalRef: any = useRef(null);
