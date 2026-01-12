d227369daca887a2962d18d15b0d936b62c5ef13
egileekada <egileoniso.ekada@gmail.com>
2026-01-12 11:33:00 +0100

    Update UserId component to align bio text to the left for improved readability and visual consistency.

--- src/page/user.tsx
+++ src/page/user.tsx
@@ -52,7 +52,7 @@ export default function UserId() {
                             <div className=" px-[14px] py-[8px] font-extrabold text-sm rounded-3xl bg-[#37137F] text-white shadow " >
                                 About Us
                             </div>
-                            <p className=" text-sm font-medium " >{textLimit(data?.bio, 150)} {data?.bio?.length > 150 && <span onClick={() => setShow(true)} className=" font-bold cursor-pointer underline " >Show More</span>}</p>
+                            <p className=" text-sm font-medium text-left " >{textLimit(data?.bio, 150)} {data?.bio?.length > 150 && <span onClick={() => setShow(true)} className=" font-bold cursor-pointer underline " >Show More</span>}</p>
                         </div>
                         <div className=" w-full flex flex-col items-center gap-2 " >
                             <div className=" p-3 font-extrabold text-sm rounded-lg text-white bg-[#B00062] shadow " >
